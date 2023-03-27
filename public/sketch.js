768d158f84fa9541aabd6d444010211e7b8426d9
Nathan Phan <nathan.phan2320@gmail.com>
2023-03-28 10:34:36 +1100

    base idea

--- public/sketch.js
+++ public/sketch.js
@@ -1,11 +1,25 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
+
 function setup () {
-   createCanvas (innerWidth, innerHeight)
+   createCanvas (576, 324)
+   stroke ('white')
 }
 
-function  draw () {
-   background (`turquoise`)
-   square (width / 2, height / 2, 100)
+function draw () {
+   background ('black')
+
+   beginShape ()
+   for (let i = 0; i < 5; i++) {
+   const cnr = rand_coordinate ()
+   vertex (cnr.x, cnr.y)
+   }
+   
+   endShape ()
 }
+
+function rand_coordinate () {
+   return { x: random (width), y: random (height) }
+}
+   
