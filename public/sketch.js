8d2c53789654a580638070d700cd94981d422d43
Nathan Phan <nathan.phan2320@gmail.com>
2023-03-28 12:45:36 +1100

    net art changes

--- public/sketch.js
+++ public/sketch.js
@@ -1,25 +1,60 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
+let fruitsDonut
+
+let counter = 0
+let myColour
+
+
+//function preload() {
+//   fruitsDonut = loadFont ('fonts/Fruits Donut.ttf');
+//}
 
 function setup () {
    createCanvas (576, 324)
-   stroke ('white')
+   stroke ('black')
+   myColour = color(random(255), random(255), random(255))
+   frameRate(100)
 }
 
 function draw () {
-   background ('black')
+   background ("black")
 
-   beginShape ()
-   for (let i = 0; i < 5; i++) {
-   const cnr = rand_coordinate ()
-   vertex (cnr.x, cnr.y)
-   }
-   
-   endShape ()
+   change_color()
+   fill(myColour)   
+   draw_shape ();
+
+   fill ("black")
+   noStroke ()
+   //textFont (fruitsDonut)
+   textSize (100)
+   textAlign (CENTER)
+   text (`RMIT`, 288, 130)
+
+   textSize(60)
+   text ('Creative Coding', 288, 190)
+   text ('Specialisation', 288, 250)
+
+   counter = counter + 1
 }
 
 function rand_coordinate () {
-   return { x: random (width), y: random (height) }
+  return coordinate = { x1: random (width), y1: random (height) }
 }
    
+function draw_shape () {
+   beginShape ()
+   for (let i = 0; i < 20; i++) {
+   const cnr = rand_coordinate ()
+   vertex (cnr.x1, cnr.y1)
+   }
+   endShape ()
+}
+
+function change_color () {
+   if (counter > 100) {
+      myColour = color(random(255), random(255), random(255))
+      counter = 0
+   }
+}
